84cd133e96d3ebc135192f4dbdc57af7003a186f
karim-lucky <karimccw8100@gmail.com>
2025-06-20 11:31:19 +0500

    ddddddddd

--- abc.js
+++ abc.js
@@ -1,5 +1,9 @@
 export default function (){
     return <div>
-        Karim Practice 
+        Karim Practice
+        dfdsf
+        dfdsfdf 
+        ikram khan wazir shakai kairm khan wazir shakai waziristan done 
     </div>
+    
 }
